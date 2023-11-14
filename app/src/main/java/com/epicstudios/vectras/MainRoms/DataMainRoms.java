7293722ddcc215d9947cf541820f897538413046
Epic Studios <79816069+epicstudios856@users.noreply.github.com>
2023-11-14 22:47:07 +0200

    permissions fix

---
+++ app/src/main/java/com/epicstudios/vectras/MainRoms/DataMainRoms.java
@@ -0,0 +1,7 @@
+package com.epicstudios.vectras.MainRoms;
+public class DataMainRoms {
+    public String itemIcon;
+    public String itemName;
+    public String itemPath;
+    public String itemExtra;
+}
