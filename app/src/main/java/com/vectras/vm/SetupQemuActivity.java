914375c8edc5274e50f81dedacbff725e1f8c2bd
Epic Studios <79816069+epicstudios856@users.noreply.github.com>
2024-04-18 23:50:16 +0200

    change libjack to jack

--- app/src/main/java/com/vectras/vm/SetupQemuActivity.java
+++ app/src/main/java/com/vectras/vm/SetupQemuActivity.java
@@ -353,7 +353,7 @@ public class SetupQemuActivity extends AppCompatActivity implements View.OnClick
                 " apk update;" +
                 " apk add tar tigervnc dwm libslirp libslirp-dev pulseaudio-dev glib-dev pixman-dev zlib-dev spice-dev" +
                 " libusbredirparser usbredir-dev libiscsi-dev  sdl2 sdl2-dev libepoxy-dev virglrenderer-dev rdma-core" +
-                " libusb ncurses-libs curl libnfs sdl2 gtk+3.0 fuse libpulse libseccomp libjack pipewire liburing;" +
+                " libusb ncurses-libs curl libnfs sdl2 gtk+3.0 fuse libpulse libseccomp jack pipewire liburing;" +
                 " tar -xzvf " + tarPath + " -C /;" +
                 " rm " + tarPath + ";" +
                 " mkdir -p ~/.vnc && echo -e \"555555\\n555555\" | vncpasswd -f > ~/.vnc/passwd && chmod 0600 ~/.vnc/passwd" +
