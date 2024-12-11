1c5bd6488e6a791da0eaf16687a91d2d042817b8
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-12-11 13:30:12 +0700

    French toast

--- app/src/main/java/com/vectras/vm/RomInfo.java
+++ app/src/main/java/com/vectras/vm/RomInfo.java
@@ -8,6 +8,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.DocumentsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
