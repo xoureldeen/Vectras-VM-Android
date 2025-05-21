8a1bbf4b0a14ca07719c415954eec8e1b35e5ecd
Veevee3124 <levee684@gmail.com>
2025-05-20 19:47:18 -0700

    adfwf

--- terminal-emulator/src/main/java/com/termux/terminal/EmulatorDebug.java
+++ terminal-emulator/src/main/java/com/termux/terminal/EmulatorDebug.java
@@ -1,10 +1,1 @@
-package com.termux.terminal;
-
-import android.util.Log;
-
-public final class EmulatorDebug {
-
-    /** The tag to use with {@link Log}. */
-    public static final String LOG_TAG = "termux";
-
-}
+saa
