c7438dfe7863868d984a5f56615a87986ad02187
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-07-18 22:05:39 +0700

    Update

--- app/src/main/java/com/vectras/vm/SetArchActivity.java
+++ app/src/main/java/com/vectras/vm/SetArchActivity.java
@@ -4,44 +4,66 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.vectras.qemu.MainSettingsManager;
+import com.vectras.vm.databinding.ActivitySetArchBinding;
+import com.vectras.vm.utils.PackageUtils;
 import com.vectras.vm.utils.UIUtils;
+
+import java.util.Objects;
 
 public class SetArchActivity extends AppCompatActivity implements View.OnClickListener {
 
     SetArchActivity activity;
+    ActivitySetArchBinding binding;
     private static Handler mHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mHandler = new Handler();
         UIUtils.edgeToEdge(this);
-        setContentView(R.layout.activity_set_arch);
-        UIUtils.setOnApplyWindowInsetsListener(findViewById(R.id.main));
+        binding = ActivitySetArchBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        UIUtils.setOnApplyWindowInsetsListener(findViewById(R.id.main));
         activity = this;
-        Button archi386 = findViewById(R.id.archi386);
-        Button archx86_64 = findViewById(R.id.archx86_64);
-        Button archarm64 = findViewById(R.id.archarm64);
-        Button archppc = findViewById(R.id.archppc);
-        Button web = findViewById(R.id.webBtn);
-        Button buttongetcm = findViewById(R.id.buttongetcm);
-        CardView cdCustom = findViewById(R.id.cdCustom);
-        archi386.setOnClickListener(this);
-        archx86_64.setOnClickListener(this);
-        archarm64.setOnClickListener(this);
-        archppc.setOnClickListener(this);
-        web.setOnClickListener(this);
-        buttongetcm.setOnClickListener(this);
-        cdCustom.setOnClickListener(this);
+        binding.archi386.setOnClickListener(this);
+        binding.archx8664.setOnClickListener(this);
+        binding.archarm64.setOnClickListener(this);
+        binding.archppc.setOnClickListener(this);
+        binding.webBtn.setOnClickListener(this);
+        binding.buttongetcm.setOnClickListener(this);
+        binding.bntimport.setOnClickListener(this);
+
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setNavigationOnClickListener(v -> {
+            onBackPressed();
+        });
+
+        binding.toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.roms_store) {
+
+                Intent intent = new Intent(this, RomsManagerActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            }
+            return false;
+        });
+
+        if (PackageUtils.isInstalled("com.anbui.cqcm.app", this)) {
+            binding.buttongetcm.setText(getResources().getString(R.string.open));
+        }
     }
 
     public void onClick(View v) {
@@ -72,16 +94,29 @@ public class SetArchActivity extends AppCompatActivity implements View.OnClickLi
             q.setData(Uri.parse(qe));
             startActivity(q);
         } else if (id == R.id.buttongetcm) {
-            Intent intent = new Intent();
-            intent.setAction(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse("android-app://com.anbui.cqcm.app"));
-            startActivity(intent);
-        } else if (id == R.id.cdCustom) {
+            PackageManager pm = getPackageManager();
+            Intent intent = pm.getLaunchIntentForPackage("com.anbui.cqcm.app");
+
+            if (intent != null) {
+                startActivity(intent);
+            } else {
+                Intent intenturl = new Intent();
+                intenturl.setAction(Intent.ACTION_VIEW);
+                intenturl.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.anbui.cqcm.app"));
+                startActivity(intenturl);
+            }
+        } else if (id == R.id.bntimport) {
             Intent intent = new Intent();
             intent.setClass(getApplicationContext(), CustomRomActivity.class);
             intent.putExtra("importcvbinow", "");
             startActivity(intent);
             finish();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.set_arch_toolbar_menu, menu);
+        return true;
     }
 }
