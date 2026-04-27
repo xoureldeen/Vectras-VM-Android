63920a0c862bec916134e7bcd5b0df1711e27983
An Bui <91354810+AnBui2004@users.noreply.github.com>
2026-04-27 13:17:06 +0700

    4.0.4
    - Added a stretch mode to fit the screen for VNC.

--- app/src/main/java/com/vectras/qemu/VNCConfig.java
+++ app/src/main/java/com/vectras/qemu/VNCConfig.java
@@ -3,4 +3,5 @@ package com.vectras.qemu;
 public class VNCConfig {
     public static final int fitToScreen = 0;
     public static final int oneToOne = 1;
+    public static final int scaleToFitScreen = 2;
 }
