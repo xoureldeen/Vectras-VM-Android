307291c5f4bb8959cfe9f2ba2b716f94f6173f54
An Bui <91354810+AnBui2004@users.noreply.github.com>
2026-05-30 17:18:27 +0700

    4.2.1
    - Improved Settings.
    - Added option to receive notifications about suggestions and tips.

---
+++ app/src/main/java/com/vectras/vm/settings/Settings2Activity.java
@@ -0,0 +1,45 @@
+package com.vectras.vm.settings;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.vectras.qemu.MainSettingsManager;
+import com.vectras.vm.databinding.ActivitySettings2Binding;
+import com.vectras.vm.utils.UIUtils;
+
+import java.util.Objects;
+
+public class Settings2Activity extends AppCompatActivity {
+
+    ActivitySettings2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        UIUtils.edgeToEdge(this);
+        binding = ActivitySettings2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        binding.toolbar.setNavigationOnClickListener(view -> finish());
+        initialize();
+    }
+
+    private void initialize() {
+        binding.lnTheme.setOnClickListener(v -> startActivity(new Intent(this, ThemeActivity.class)));
+        binding.lnNotification.setOnClickListener(v -> startActivity(new Intent(this, NotificationSettingsActivity.class)));
+        binding.lnSystem.setOnClickListener(v -> startActivity(new Intent(this, SystemSettingsActivity.class)));
+
+        binding.lnQemu.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainSettingsManager.class);
+            intent.putExtra("goto", "qemu");
+            startActivity(intent);
+        });
+        binding.lnImportExport.setOnClickListener(v -> startActivity(new Intent(this, ImportExportSettingsActivity.class)));
+
+        binding.lnVnc.setOnClickListener(v -> startActivity(new Intent(this, VNCSettingsActivity.class)));
+        binding.lnX11.setOnClickListener(v -> startActivity(new Intent(this, X11DisplaySettingsActivity.class)));
+    }
+}
